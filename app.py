89924e7f095e89ddab1e3954ee6a453f128d8605
mitha <mithashrivas24@gmail.com>
2026-07-14 09:53:53 +0530

    Fix Render port

--- app.py
+++ app.py
@@ -90,12 +90,8 @@ def predict():
         confidence=confidence
     )
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
